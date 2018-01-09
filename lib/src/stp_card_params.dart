aa620a4db3da2be6d94a12f01390d520ee164822
Taylor Smith <tsmith1024@gmail.com>
2018-01-09 16:13:13 -0600

    Add toMap method on card_params to map data for API use

--- lib/src/stp_card_params.dart
+++ lib/src/stp_card_params.dart
@@ -10,14 +10,47 @@ class STPCardParams {
   STPAddress address;
 
   STPCardParams(
+    this.number,
     this.expMonth,
     this.expYear,
     {
-      this.number,
       this.cvc,
       this.name,
       this.currency: 'usd',
       this.address
     }
   );
+
+  Map<String, Object> toMap() {
+    var map = {
+      "number": number,
+      "exp_month": expMonth,
+      "exp_year": expYear,
+      "cvc": cvc,
+      "name": name,
+      "currency": currency,
+    };
+
+    if (address != null) {
+      map["address_line1"] = address.line1;
+      map["address_line2"] = address.line2;
+      map["address_city"] = address.city;
+      map["address_state"] = address.state;
+      map["address_zip"] = address.postalCode;
+      map["address_country"] = address.country;
+    }
+
+    var deleteKeys = [];
+    map.forEach((key, value) {
+      if (value == null) {
+        deleteKeys.add(key);
+      };
+    });
+
+    for (var key in deleteKeys) {
+      map.remove(key);
+    }
+
+    return map;
+  }
 }
