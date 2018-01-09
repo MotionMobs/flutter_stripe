2acc12b605f024889d859d40b5ccaca8b4d737f4
Taylor Smith <tsmith1024@gmail.com>
2018-01-09 16:11:36 -0600

    Add stripe token generation test

--- test/flutter_stripe_test.dart
+++ test/flutter_stripe_test.dart
@@ -1,13 +1,27 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:test/test.dart';
 
 import 'package:flutter_stripe/flutter_stripe.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = new Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+Future main() {
+  test('requests token successfully', () async {
+    Map<String, String> envVars = Platform.environment;
+    final client = new STPApiClient(envVars['STRIPE_KEY']);
+    final card = new STPCardParams(
+      '4242424242424242',
+      01,
+      2019,
+      cvc: '123',
+      address: new STPAddress(
+        line1: "123 Test St",
+        city: "Somewhere",
+        state: "CA",
+        postalCode: "11111",
+      )
+    );
+
+    final token = await client.createTokenWithCard(card);
+    expect(token is STPToken, true);
   });
 }
